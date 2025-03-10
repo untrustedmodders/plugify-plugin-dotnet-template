ae7afdeb85c1c34797c02f26ebd83f417590eba6
Nikita Ushakov <nikita.ushakov0000@outlook.com>
2025-03-10 01:44:56 +0000

    initial commit

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using Plugify;
+
+namespace ExampleNamespace
+{
+    public class ExamplePlugin : Plugin
+    {
+        public void OnStart()
+        {
+            Console.WriteLine(".NET: OnStart");
+        }
+        
+        /*public void OnUpdate(float dt) 
+        {
+            Console.WriteLine(".NET: OnUpdate");
+        }*/
+
+        public void OnEnd()
+        {
+            Console.WriteLine(".NET: OnEnd");
+        }
+        
+        public static void MakePrint(int count, string message) 
+        {
+            for (int i = 0; i < count; ++i) {
+                Console.WriteLine(message);
+            }
+        }
+    }
+}
